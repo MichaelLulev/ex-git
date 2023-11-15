e7859a1cb6d2c787464dbd06bb48816ef02ff00a
Michael Lulev <miki1990@gmail.com>
2023-11-16 00:09:54 +0200

    Add getRandomColor() to js/utils.js

--- js/utils.js
+++ js/utils.js
@@ -3,3 +3,10 @@
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
+
+function getRandomColor() {
+    const r = getRandomInt(0, 256)
+    const g = getRandomInt(0, 256)
+    const b = getRandomInt(0, 256)
+    return `rgb(${r}, ${g}, ${b})`
+}

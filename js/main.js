d7f8fce1bc2104e3fa547963fbd4c9cf43929fb7
Michael Lulev <miki1990@gmail.com>
2023-11-15 23:50:28 +0200

    Limit ball size to 400px and reset to 100px

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var ballSize = 100
 
 function onBallClick(elBall) {
     ballSize += 50
+    if (400 < ballSize) ballSize = 100
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.innerText = ballSize

75731a66f596ea4b5ea4e1167232db3e4b1afa6e
Michael Lulev <miki1990@gmail.com>
2023-11-16 00:23:00 +0200

    Add getRandomIntInclusive() to js/utils.js

--- js/utils.js
+++ js/utils.js
@@ -4,6 +4,10 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
+function getRandomIntInclusive(min, max) {
+    return getRandomInt(min, max + 1)
+}
+
 function getRandomColor() {
     const r = getRandomInt(0, 256)
     const g = getRandomInt(0, 256)

94988aa72d1872199e060eb1c6fce45065bc9e4f
Michael Lulev <miki1990@gmail.com>
2023-11-16 00:24:15 +0200

    Make the ball change random size and color

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var ballSize = 100
+var ballColor
 
 function onBallClick(elBall) {
-    ballSize += 50
+    ballSize += getRandomIntInclusive(20, 60)
+    ballColor = getRandomColor()
     if (400 < ballSize) ballSize = 100
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
+    elBall.style.backgroundColor = ballColor
     elBall.innerText = ballSize
 }

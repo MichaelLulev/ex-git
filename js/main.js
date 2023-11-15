df431e74e42512826bda2c9be382019254e28b88
Michael Lulev <miki1990@gmail.com>
2023-11-15 23:34:23 +0200

    Add grow the ball when user clicks it

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-function onBallClick() {
-    console.log('The ball was clicked! :)')
+var ballSize = 100
+
+function onBallClick(elBall) {
+    ballSize += 50
+    elBall.style.width = ballSize + 'px'
+    elBall.style.height = ballSize + 'px'
+    elBall.innerText = ballSize
 }

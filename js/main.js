516a4fc5818c836ff10fea107ebf79d4264580ea
Michael Lulev <miki1990@gmail.com>
2023-11-16 08:09:49 +0200

    Make fourth ball shrink first two balls

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 'use strict'
 
+var isGrow = true
+
 function onBallClick(elBall, maxDiameter) {
     var ballSize = +elBall.dataset.size
-    ballSize += getRandomIntInclusive(20, 60)
+    const diff = getRandomIntInclusive(20, 60)
+    ballSize += isGrow ? +diff : -diff
     if (maxDiameter < ballSize) ballSize = 100
+    if (ballSize < 100) ballSize = 100
     const ballColor = getRandomColor()
     elBall.dataset.size = ballSize
     elBall.style.width = ballSize + 'px'
@@ -14,6 +18,12 @@ function onBallClick(elBall, maxDiameter) {
 
 function onThirdBallClick() {
     const firstTwoBalls = document.querySelectorAll('.ball-1, .ball-2')
-    console.log(firstTwoBalls)
     firstTwoBalls.forEach(ball => ball.click())
+}
+
+function onFourthBallClick() {
+    const firstTwoBalls = document.querySelectorAll('.ball-1, .ball-2')
+    isGrow = false
+    firstTwoBalls.forEach(ball => ball.click())
+    isGrow = true
 }
